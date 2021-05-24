f37fc7914311ec1f27e5c790a654cf6ec77a2dba
Imer <Imer@DESKTOP-61HL19C>
2021-05-24 17:47:18 -0600

    -Broker and agent assigned in new lead.

--- Realestate_portal/Controllers/CustomersController.cs
+++ Realestate_portal/Controllers/CustomersController.cs
@@ -58,7 +58,7 @@ namespace Realestate_portal.Controllers
                 ViewBag.notifications = lstAlerts;
                 ViewBag.userID = activeuser.ID_User;
                 ViewBag.userName = activeuser.Name + " " + activeuser.LastName;
-                ViewBag.userslist = (from u in db.Sys_Users where u.Sys_Company.ID_Company == activeuser.ID_Company && u.Roles== "Agent" select u).ToList();
+                ViewBag.userslist = (from u in db.Sys_Users where u.Sys_Company.ID_Company == activeuser.ID_Company && (u.Roles== "Agent" || u.Roles =="Admin")select u).ToList();
 
                 ViewBag.rol = "";
 
