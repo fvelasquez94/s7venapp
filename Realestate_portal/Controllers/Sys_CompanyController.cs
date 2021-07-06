13ed1d2555ee75dc76a4e524cbd7d43a7cd31a49
Imer <Imer@DESKTOP-61HL19C>
2021-07-06 17:08:53 -0600

    -Fix broker creation

--- Realestate_portal/Controllers/Sys_CompanyController.cs
+++ Realestate_portal/Controllers/Sys_CompanyController.cs
@@ -131,7 +131,7 @@ namespace Realestate_portal.Controllers
         [HttpPost]
         public ActionResult Create([Bind(Include = "ID_Company,Name,Web,ShortName")] Sys_Company sys_Company, HttpPostedFileBase logo, string email, string information)
         {
-            sys_Company.Logo = "";
+            
             if (sys_Company.Web == null) { sys_Company.Web = ""; }
             if (sys_Company.ShortName == null) { sys_Company.ShortName = ""; }
             if (ModelState.IsValid)
